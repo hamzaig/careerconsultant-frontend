a54c4176081b04f6b7c9cdd1f4dc08a4326d0e5a
Hamza Ali Khalid <50531566+hamzaig@users.noreply.github.com>
2024-05-18 05:17:21 +0500

    x

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="xvIMB_dzO5SPCFZLabuJ9VkiTFZxkEoKqBErxNrrvqM"
+        />
+        <body className={inter.className}>{children}</body>
+      </Head>
     </html>
   );
 }
